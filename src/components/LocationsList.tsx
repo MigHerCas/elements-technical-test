849524c8e00443e96705922c48c50cbd2a45a9fc
Miguel Hernanz <migherdcas@gmail.com>
2021-01-23 15:45:33 +0100

    feat(React): Location item layers + icons.

--- src/components/LocationsList.tsx
+++ src/components/LocationsList.tsx
@@ -2,6 +2,10 @@ import { LocationName } from '@/models';
 import Link from 'next/link';
 import Image from 'next/image';
 
+// Icons
+import Eye from '../../public/icons/eye.svg';
+// import EyeCrossed from '../../public/icons/eye-crossed.svg';
+
 interface Props {
   locations: Set<LocationName>;
 }
@@ -15,16 +19,29 @@ const LocationsList = ({ locations }: Props): JSX.Element => {
       <ol className="locations-list">
         <li className="location-item">
           <Link href="/barcelona">
-            <div className="location-item__back-layer">
-              <div className="location-item__image">
-                <Image
-                  width={300}
-                  height={200}
-                  src="https://firebasestorage.googleapis.com/v0/b/mobile-assignment-server.appspot.com/o/barcelona.jpg?alt=media&token=78363b9d-3c33-432c-9bf8-f60786153b13"
-                />
+            <div>
+              <div className="location-item__front-layer">
+                <div className="location-item__hover-layer">
+                  <div className="icon-wrapper">
+                    <Eye />
+                  </div>
+                  <span>Show</span>
+                </div>
+                <div className="location-item__toggle">
+                  <Eye />
+                </div>
               </div>
-              <div className="location-item__name">
-                <span>Barcelona</span>
+              <div className="location-item__back-layer">
+                <div className="location-item__image">
+                  <Image
+                    width={300}
+                    height={200}
+                    src="https://firebasestorage.googleapis.com/v0/b/mobile-assignment-server.appspot.com/o/barcelona.jpg?alt=media&token=78363b9d-3c33-432c-9bf8-f60786153b13"
+                  />
+                </div>
+                <div className="location-item__name">
+                  <span>Barcelona</span>
+                </div>
               </div>
             </div>
           </Link>
